d2b9882ebad86c59be26f297170b974ca52ca9c3
Jamie Stuart <jamesnapierstuart@users.noreply.github.com>
2015-09-22 11:22:56 +0100

    Changed MouseX to 20 so it can be viewed on mobile moving

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ function drawSineCircle() {
 	translate(width/2, height/2)
 	beginShape()
 	for(i = 0; i < TWO_PI; i+=0.01) {
-		u = map(noise(t), 0, 1, 0, mouseX)
+		u = map(noise(t), 0, 1, 0, 20)
 		r = map(sin(i*u), 0, 1, windowWidth/8.4, windowWidth/8.15)	 // now we map the value of each point (which is -1 to 1 from the sin function and then map 50 100) The value won't be going up fast enough so multiply by 5
 		x = r*cos(i)
 		y = r*sin(i)
