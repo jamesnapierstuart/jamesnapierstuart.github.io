4e2c8b50ad9f95dcb192fd2ac532984d039504a4
Jamie Stuart <jamesnapierstuart@users.noreply.github.com>
2015-09-22 09:25:47 +0100

    Resize window works, alpha image improved

--- sketch.js
+++ sketch.js
@@ -1,88 +1,58 @@
 var t = 0
-var rate = 0
-var song
-
-function preload() {
-	// song = loadSound('island.wav');
-}
-
 
 function setup() {
-	createCanvas(window.innerWidth, window.innerHeight)
+	createCanvas(windowWidth, windowHeight)
 	background(0)
 	setFrameRate(8)
-	// song.loop()
+	print("Window Width and Height")
+	print(windowWidth, windowHeight)
 }
 
 function draw() {
-	// background(0) // Take this off if want to take a serious of pictures of the events
+	background(0, 0, 0, 40)
+	// drawFont()
+	drawSineCircle()
+}
 
-	// speed = map(mouseY, 0.1, height, 0.01, 4)
-	// song.rate(speed)
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+  print("The window is being resized")
+  background(0)
+  drawSineCircle()
+  // drawFont()
+}
 
-	rate++
-	// if(rate > touchY) {
-	// 	background(0)
-	// 	rate = 0
-	// }
-	if(rate > mouseY) {
-		background(0)
-		rate = 0
-	}
-
-
-	
-	// stroke(255)	
+function drawSineCircle() {
 	strokeWeight(2)
-	stroke(255, 0, 255, 100)
+	stroke(0, 0, 255, 100)
 	noFill()
-	beginShape()
+	push();
 	translate(width/2, height/2)
+	beginShape()
 	for(i = 0; i < TWO_PI; i+=0.01) {
-		// r = random(100, 105) // This makes a random movement 
-		// j = i+20
 		u = map(noise(t), 0, 1, 0, mouseX)
-
-		r = map(sin(i*u), 0, 1, 200, 206)	 // now we map the value of each point (which is -1 to 1 from the sin function and then map 50 100) The value won't be going up fast enough so multiply by 5
-
-
-
+		r = map(sin(i*u), 0, 1, windowWidth/8.4, windowWidth/8.15)	 // now we map the value of each point (which is -1 to 1 from the sin function and then map 50 100) The value won't be going up fast enough so multiply by 5
 		x = r*cos(i)
 		y = r*sin(i)
-		vertex(x, y, 16, 16)
+		vertex(x, y)
+
+		// original values
+		// windowWidth/8.4, windowWidth/8.15
+
+		// now change mouseX to something sensible
 	}
 	endShape(CLOSE) // Closes any gaps
-
+	pop();
 	t+=0.01
 }
 
-
-function mousePressed() {
-	background(0)
+function drawFont(){
+	s = "Yanaphauna"
+	noStroke()
+	fill(100)
+	textSize(24)
+	textAlign(CENTER)
+	textStyle(NORMAL)
+	textFont("Helvetica")
+	text(s, windowWidth/2, windowHeight-windowHeight/8, 100, 100) // Text wraps within text box
 }
-
-// function doOnOrientationChange()
-//   {
-//     switch(window.orientation) 
-//     {  
-//       case -90:
-//       // Need to change the code here translate it????
-//       background(0)
-//       translate(window.width/2, window.height/2)
-//       case 90:
-//       // Translate the code back again????
-//       background(0)
-//       translate(window.width/2, window.height/2)
-//       // Or just have the screen locked down?
-//         break; 
-//       default:
-//         break; 
-//     }
-//   }
-
-//   window.addEventListener('orientationchange', doOnOrientationChange);
-
-
-//   function windowResized() {
-//   resizeCanvas(window.innerWidth, window.innerHeight);
-// }
