5dbe38d23597faf9ff385d8e466f0efa26414944
JamieStuart <jamesnapierstuart@gmail.com>
2016-06-02 02:56:47 +0100

    New approach orientation
    New approach orientation change

--- sketch.js
+++ sketch.js
@@ -4,15 +4,12 @@
 
 // BUT it create a whole bunch of problems where the sketches weren't big enough
 
+// much easier would be to say, if the orientation has changed then change the inner window width
+
 var sketchOne = function(p) {
 
-	var previousWidth = p.displayHeight
-	var previousHeight = p.displayWidth
-
-	var change = false
-
-	p.x = p.displayWidth / 2
-	p.y = p.displayHeight / 2
+	p.x = p.windowWidth / 2
+	p.y = p.windowHeight / 2
 
 	p.bgColor = p.random(200)
 
@@ -21,8 +18,8 @@ var sketchOne = function(p) {
 	p.b = 0
 	p.a = 25
 
-	p.widthSize = p.displayWidth
-	p.heightSize = p.displayHeight
+	p.widthSize = p.windowWidth
+	p.heightSize = p.windowHeight
 
 	p.setup = function() {
 		p.createCanvas(p.widthSize, p.heightSize)
@@ -39,24 +36,15 @@ var sketchOne = function(p) {
 	}
 
 	p.windowResized = function() {
-
-
-		if(!change)
-		{
-			console.log('size changed')
-			p.resizeCanvas(p.displayHeight, p.displayWidth) // this is nasty
-		}
-		else
-		{
-			console.log('size changed')
-			p.resizeCanvas(p.displayWidth, p.displayHeight)
-		}
-
-		 change = !change
-		
-  		
+		p.resizeCanvas(p.windowWidth, p.windowHeight)
   		p.background(p.bgColor)
   		p.x = p.width/2
+		p.y = p.height/2
+	}
+
+	p.orientationchange = function() {
+		p.resizeCanvas(p.windowHeight, p.windowWidth)
+		p.x = p.width/2
 		p.y = p.height/2
 	}
 }
@@ -85,7 +73,19 @@ setInterval(resetBackground, 3000)
 
 
 
+window.addEventListener("orientationchange", function() {
+    alert("the orientation of the device is now " + screen.orientation.angle);
 
+    for(var i = 0; i < 20; i++)
+	{
+		sketches[i].x = sketches[i].width/2
+		sketches[i].y = sketches[i].height/2
+		sketches[i].r = sketches[i].random(255)
+		sketches[i].g = sketches[i].random(255)
+		sketches[i].bgColor = sketches[i].random(100)
+		sketches[i].background(sketches[i].bgColor)
+	}
+});
 
 
 
