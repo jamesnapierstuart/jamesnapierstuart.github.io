b77813742ff4317a84d23f0eea24cb4c25a21031
Jamie Stuart <jamesnapierstuart@users.noreply.github.com>
2016-06-02 13:45:15 +0100

    Adding in text test

--- sketch.js
+++ sketch.js
@@ -36,6 +36,13 @@ var sketchOne = function(p) {
 
 		p.x = p.x + p.random(-10, 10)
 		p.y = p.y + p.random(-10, 10)
+		
+		textSize(32);
+		text("word", 10, 30);
+		fill(0, 102, 153);
+		text("word", 10, 60);
+		fill(0, 102, 153, 51);
+		text("word", 10, 90);
 	}
 
 	p.windowResized = function() {
